29116a1f739cfd99247d27b0665441569f0440f5
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-08-19 07:52:48 -0700

    created the displayDEtails

--- beautifulpictures/src/App.jsx
+++ beautifulpictures/src/App.jsx
@@ -8,11 +8,13 @@ const Key = "MfcgBp0ax9DY23ItLMgQOHpKXUhbbGrcyPlIeNdHVzTqgZhrfPz11Z9P";
 export default function App(){
   const [query, setQuery] = useState("")
   const [images, setImages] =useState([])
+  const [selected, setSelected] = useState(null)
 
   useEffect(function(){
     async function fetchImages(){
+      if(!query) return;
       const res = await fetch(
-        `https://api.pexels.com/v1/search?query=${query}&per_page=1`,
+        `https://api.pexels.com/v1/search?query=${query}`,
         {
       headers: {
         Authorization: "MfcgBp0ax9DY23ItLMgQOHpKXUhbbGrcyPlIeNdHVzTqgZhrfPz11Z9P"
@@ -26,10 +28,14 @@ export default function App(){
     if(query) fetchImages()
   }, [query])
 
+   function displayDetails(id){
+    setSelected(id)
+   }
   return(
     <div>
       <Form query={query} setQuery={setQuery}/>
-      <Images images={images} setImages={setImages}/>
+      <Images images={images} setImages={setImages} ondisplayDetails={displayDetails}/>
+      <ImageDetails selected={selected} setSelected={setSelected} />
     </div>
   )
 }
@@ -46,20 +52,50 @@ function Form({query, setQuery}){
     </div>
   )
 }
-function Images({images,setImages}){
+function Images({images,setImages, ondisplayDetails}){
   return(
     <div>
 {images.map((image)=>(
-  <Image key={image.id} image={image} />
+  <Image key={image.id} image={image} ondisplayDetails={ondisplayDetails}/>
 ))}
     </div>
   )
 }
 
-function Image({image}){
+function Image({image, ondisplayDetails}){
+  return(
+    <div onClick={()=>ondisplayDetails(image.id)}>
+     <img src={image.src.small} 
+     alt={image.alt}
+     className='w-[100px]'
+     />
+     <p>{image.photographer}</p>
+    </div>
+  )
+}
+
+function ImageDetails({selected, setSelected}){
+const [image, setImage] = useState({})
+
+
+  useEffect(function(){
+    async function displayDetails(){
+     const res = await fetch(`https://api.pexels.com/v1/photos/${selected}`,
+     {
+      headers : {
+      Authorization:"MfcgBp0ax9DY23ItLMgQOHpKXUhbbGrcyPlIeNdHVzTqgZhrfPz11Z9P"
+      }
+     }
+    )
+     const data= await res.json()
+     console.log(data)
+     setImage(data)
+    }
+    displayDetails() 
+  }, [selected])
   return(
     <div>
-{}
+  <p>{image.photographer}</p>
     </div>
   )
 }
